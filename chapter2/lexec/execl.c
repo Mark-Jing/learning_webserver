326d7b42879ee40c79ec45ca76fb09a5193fd3b7
jingxiaokang <1320114246@qq.com>
2023-09-26 23:13:23 +0800

    exec

--- chapter2/lexec/execl.c
+++ chapter2/lexec/execl.c
@@ -1,7 +1,17 @@
 #include <unistd.h>
 #include <stdio.h>
+#include <sys/types.h>
 
 int main() {
-    
+    pid_t pid = fork();
+    if(pid > 0) {
+        printf("in parent process, pid = %d\n", getpid());
+    } else if(pid == 0) {
+        execl("hello", "hello", NULL);
+        printf("in child process");
+    }
+    for(int i = 0; i <= 3; i++) {
+        printf("i = %d, pid = %d\n", i, getpid());
+    }
     return 0;
 }
